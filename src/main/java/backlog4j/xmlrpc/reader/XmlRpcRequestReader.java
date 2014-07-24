489f3af026ea6e86950db194793b87d5a0f4f32b
eguchi <eguchi@nulab.co.jp>
2014-07-24 16:39:27 +0900

    improve exception

--- src/main/java/backlog4j/xmlrpc/reader/XmlRpcRequestReader.java
+++ src/main/java/backlog4j/xmlrpc/reader/XmlRpcRequestReader.java
@@ -1,5 +1,6 @@
 package backlog4j.xmlrpc.reader;
 
+import backlog4j.BacklogException;
 import backlog4j.xmlrpc.XmlRpcException;
 
 import javax.xml.parsers.SAXParser;
@@ -14,31 +15,28 @@ public class XmlRpcRequestReader {
 
     public static Object read(InputStream input) {
 
-        XmlRpcHandler handler;
         try {
+
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser parser = factory.newSAXParser();
 
-            handler = new XmlRpcHandler();
+            XmlRpcHandler handler = new XmlRpcHandler();
             parser.parse(input, handler);
 
+            if (handler.isFault()) {
+                Map<String, Object> map = (Map<String, Object>) handler.getObject();
+
+                Integer faultCode = (Integer) map.get("faultCode");
+                String faultString = (String) map.get("faultString");
+
+                throw new XmlRpcException(faultCode, faultString);
+            }
+
+            return handler.getObject();
+
         } catch (Exception e) {
-            throw new XmlRpcException(e);
+            throw new BacklogException(e);
         }
-
-        if (handler.isFault()) {
-            Map<String, Object> map = (Map<String, Object>) handler.getObject();
-
-            Integer faultCode = (Integer) map.get("faultCode");
-            String faultString = (String) map.get("faultString");
-
-            throw new XmlRpcException(faultCode, faultString);
-        }
-
-
-        return handler.getObject();
-
-
     }
 
 
